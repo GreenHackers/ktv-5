6b7e90c23b548c2ceb2703282498a31e8b62809b
mart <mart@rlstudio.com>
2016-01-18 23:50:12 +0100

    Start

---
+++ app/app.js
@@ -0,0 +1,8 @@
+/*
+http://viralpatel.net/blogs/angularjs-routing-and-views-tutorial-with-example/
+ http://metanit.com/web/angular/4.1.php
+ http://job-blog.bullgare.ru/2014/01/%D1%82%D0%BE%D0%BD%D0%BA%D0%BE%D1%81%D1%82%D0%B8-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D1%8F-%D0%B1%D0%BE%D0%BB%D1%8C%D1%88%D0%BE%D0%B3%D0%BE-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD/
+ http://ruseller.com/lessons.php?rub=43&id=2303
+ http://habrahabr.ru/post/269827/
+ http://stepansuvorov.com/blog/2014/08/%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F-angularjs-right-way/
+ */
